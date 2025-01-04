aaeda6355befdf010366929126b5c7bdcbc38759
jas0ndiamond <jason.a.diamond@gmail.com>
2025-01-04 09:20:14 -0800

    add shebang and message describing wait

--- docker/redeploy.sh
+++ docker/redeploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker stop nssk-data;
 docker container rm nssk-data;
 
@@ -6,6 +8,8 @@ sudo rm -rf ./data/*;
 docker build -t nssk-mysql .;
 
 ./start.sh
+
+echo "Waiting startup to complete and deleting the setup script..."
 sleep 300
 
 docker exec -it nssk-data rm docker-entrypoint-initdb.d/0_setup.sql
