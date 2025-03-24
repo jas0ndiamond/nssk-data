afabbae859388bd0389253197244c26e7f54202c
jas0ndiamond <jason.a.diamond@gmail.com>
2025-03-23 17:59:34 -0700

    simple edits

--- src/importer/DBImporter.py
+++ src/importer/DBImporter.py
@@ -1,4 +1,5 @@
 from mysql.connector import connect, Error, IntegrityError
+
 from datetime import datetime
 
 import logging
@@ -54,6 +55,8 @@ class DBImporter:
     def set_schema_mapping(self, schema_mapping):
         self.schema_mapping = schema_mapping
 
+    # add a DataEntry object to db ingest queue. requires a schema to be set with set_schema.
+    # an optional schema mapping may be set with set_schema_mapping to resolve db fields from expected input fields
     # entry is a DataEntry object
     def add(self, entry):
 
@@ -61,7 +64,7 @@ class DBImporter:
         # is entry a subclass of DataEntry?
         # is the entry object value collection the same size as the schema?
 
-        # check if a schema is defined
+        # check if a schema or schema mapping is defined
         if self.schema is None or len(self.schema) <= 0:
             raise "Database schema must be defined"
 
@@ -161,7 +164,7 @@ class DBImporter:
                     port=int(config[DBConfig.CONFIG_PORT]),
                     user=config[DBConfig.CONFIG_USER],
                     password=config[DBConfig.CONFIG_PASS],
-                    database=config[DBConfig.CONFIG_DBASE],
+                    database=config[DBConfig.CONFIG_DBASE]
             ) as connection:
 
                 config[DBConfig.CONFIG_USER] = None
