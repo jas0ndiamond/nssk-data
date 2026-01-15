ddda532c4f6b3e41edabb4adc7ba5415f6e17ecb
jason <jason.a.diamond@gmail.com>
2026-01-15 12:18:31 -0800

    add basic wrapper script for solinst updates

---
+++ src/cosmo-solinst/run-all.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+CONF_FILE=$1
+
+if [ -z "$CONF_FILE" ]; then
+  echo "Config file required"
+  exit 1;
+fi
+
+# first batch
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG01\
+ ~/opt/nssk-data-dumps/solinst/Wagg01\ oct\ 09\ 2024\ download.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/1080831_2024_06_11\ Wagg\ 03.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/Wagg\ 03\ Oct\ 09\ 2024.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/1080931_2024_02_19.csv
+
+# jan 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG01\
+ ~/opt/nssk-data-dumps/solinst/Wagg01\ jan06\ 2025\ download.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/Wagg\ 03\ Jan\ 06\ 2025\ Download.csv
+
+# feb 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG01\
+ ~/opt/nssk-data-dumps/solinst/Wagg\ 01\ Feb\ 14\ 2025\ download.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/Wagg\ 03\ Feb\ 14\ 2025\ Download.csv
+
+# july 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG01\
+ ~/opt/nssk-data-dumps/solinst/Wagg01_1091817_2025_07_02.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/Wagg03_2130615_2025_07_01.csv
+
+# aug 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG01\
+ ~/opt/nssk-data-dumps/solinst/Wagg01_1091817__2025_08_05.csv
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/WAGG03_2130615_2025_08_05.csv
+
+# oct 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG01\
+ ~/opt/nssk-data-dumps/solinst/Wagg\ 01\ Oct\ 15\ 2025\ download.csv
+
+# nov 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+ ~/opt/nssk-data-dumps/solinst/2130615_WAGG03_2025_11_30.csv
