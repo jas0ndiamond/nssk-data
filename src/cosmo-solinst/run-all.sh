77ae2505f22607934c62015f6419b420cb8aa256
jason <jason.a.diamond@gmail.com>
2026-01-15 22:58:16 -0800

    add latest solinst data dump

--- src/cosmo-solinst/run-all.sh
+++ src/cosmo-solinst/run-all.sh
@@ -48,3 +48,7 @@ fi
 # nov 2025
 ../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
  ~/opt/nssk-data-dumps/solinst/2130615_WAGG03_2025_11_30.csv
+
+# dec 2025
+../../venv/bin/python3 ./cosmo-solinst-import.py -cfg "$CONF_FILE" WAGG03\
+~/opt/nssk-data-dumps/solinst/2130615_WAGG03_2025_12_06.csv
