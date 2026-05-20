cfb015de86ce9f3a03125359fe2e23e96ab83986
jas0ndiamond <jason.a.diamond@gmail.com>
2026-05-20 11:15:36 -0700

    address conflict

--- src/rainfall-event-data/rainfall-event-data.py
+++ src/rainfall-event-data/rainfall-event-data.py
@@ -8,6 +8,8 @@ from string import Template
 
 from mysql.connector import connect, Error
 
+from src.exception.PrecheckFailedException import PrecheckFailedException
+
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 
@@ -131,8 +133,7 @@ def precheck(conf_file):
                     cursor.fetchall()
 
                     if cursor.rowcount != 1:
-                        # TODO: custom exception
-                        raise Exception("Could not find %s target database" % target_db)
+                        raise PrecheckFailedException("Could not find %s target database" % target_db)
                     else:
                         logger.debug("Found target database %s" % target_db)
 
@@ -142,8 +143,7 @@ def precheck(conf_file):
                     cursor.fetchall()
 
                     if cursor.rowcount != 1:
-                        # TODO: custom exception
-                        raise Exception("Could not find source database %s" % SOURCE_DB_NSSK_COSMO)
+                        raise PrecheckFailedException("Could not find source database %s" % SOURCE_DB_NSSK_COSMO)
                     else:
                         logger.debug("Found source database %s" % SOURCE_DB_NSSK_COSMO)
 
@@ -153,8 +153,7 @@ def precheck(conf_file):
                     cursor.fetchall()
 
                     if cursor.rowcount != 1:
-                        # TODO: custom exception
-                        raise Exception("Could not find source database %s" % SOURCE_DB_CNV_FLOWWORKS)
+                        raise PrecheckFailedException("Could not find source database %s" % SOURCE_DB_CNV_FLOWWORKS)
                     else:
                         logger.debug("Found source database %s" % SOURCE_DB_CNV_FLOWWORKS)
 
@@ -166,8 +165,7 @@ def precheck(conf_file):
                         cursor.execute("SHOW TABLES LIKE '%s';" % sensor)
                         cursor.fetchall()
                         if cursor.rowcount != 1:
-                            # TODO: custom exception
-                            raise Exception("Could not find sensor table %s in target database" % sensor)
+                            raise PrecheckFailedException("Could not find sensor table %s in target database" % sensor)
                         else:
                             logger.debug("Found Sensor Table %s" % sensor)
 
@@ -315,8 +313,12 @@ def correlate_with_cosmo_conductance(cnv_flowworks_rainfall_timestamp, search_sp
     best_measurement_value = None
 
     for row in search_space[sensor_site]:
-        # TODO empty and None checks for row
-
+        # handle any null conductivity measurements at row[1]. database table should require non-null ActivityStartDate
+        # and ActivityStartTime at row[0]
+        # Null conductivity measurements should be excluded by query
+        if row[1] is None or row[1] == "":
+            logger.warning(f"Skipping invalid conductivity measurement at {row[0]}:[{row[1]}]")
+            continue
         # determine best result
 
         # check if the timestamp in this row is better
@@ -367,7 +369,12 @@ def correlate_with_dnv_flow_reading(cnv_flowworks_rainfall_timestamp, search_spa
     closest_timestamp_distance = 999999
 
     for row in search_space[sensor_site]:
-        # None measurements excluded by dnv database query
+        # occasional null flow measurements at row[1]. database table should require non-null MeasurementTimestamp
+        # at row[0]
+        # Null flow measurements should be excluded by query
+        if row[1] is None or row[1] == "":
+            logger.warning(f"Skipping invalid flow measurement at {row[0]}:[{row[1]}]")
+            continue
 
         # determine best result
 
