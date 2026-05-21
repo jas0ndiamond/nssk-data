f25d1a6d060de814bfb768b1cb41ce3fa87f4816
jas0ndiamond <jason.a.diamond@gmail.com>
2026-05-21 00:01:46 -0700

    thought i had already backed out these hardcoded test values

--- src/rainfall-interval-data/rainfall-interval-data.py
+++ src/rainfall-interval-data/rainfall-interval-data.py
@@ -33,10 +33,10 @@ logging.getLogger("DBImporter").setLevel(logging.INFO)
 TRACE_LOGGING = False
 
 # set these for testing specific date intervals, as the full dataset takes a long time
-#OVERRIDE_START_DATETIME = None
-#OVERRIDE_END_DATETIME = None
-OVERRIDE_START_DATETIME = "2020-01-01 00:00:00"
-OVERRIDE_END_DATETIME = "2020-04-01 00:00:00"
+OVERRIDE_START_DATETIME = None
+OVERRIDE_END_DATETIME = None
+#OVERRIDE_START_DATETIME = "2020-01-01 00:00:00"
+#OVERRIDE_END_DATETIME = "2020-04-01 00:00:00"
 
 
 # time window to search for a corresponding conductivity value
